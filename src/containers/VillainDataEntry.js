917606639800a37664133507ffab131e1e61bcb1
Lauren Hilton <lauren.hilton@daugherty.com>
2017-10-01 09:23:13 -0500

    Kieran's changes

--- src/containers/VillainDataEntry.js
+++ src/containers/VillainDataEntry.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
-import { Button, DropdownButton, Col, FormControl, ControlLabel, FieldGroup, FormGroup, Modal, MenuItem, Row, Well } from 'react-bootstrap'
+import { Button, DropdownButton, Col, FormControl, FormGroup, Modal, MenuItem, Row, Well } from 'react-bootstrap'
+//FieldGroup, ControlLabel
 
 class VillainDataEntry extends Component {
   constructor(props) {
     super(props);
-    this.state = {login: true, choosingRegion: true, showModal: false}
+    this.state = {login: true, choosingRegion: true, showModal: true}
+    this.handleGet();
   }
   toggleClick = (event) => {
     const oldRuleSet = this.state.ruleSet;
@@ -37,9 +39,7 @@ class VillainDataEntry extends Component {
       const wants = keys.filter(key => {
         return (key !== '_id' && key !== '_rev');
       })
-      this.state.regions = wants;
-      this.state.ruleSet = json;
-      console.log(this.state.ruleSet);
+      this.setState({regions:wants, ruleSet:json})
     });
   };
 
@@ -56,47 +56,47 @@ class VillainDataEntry extends Component {
 
   viewTheVillian = () => {
     this.setState({viewVillian: true})
+    this.setState({showModal: true})
   }
 
   componentWillReceiveProps(next) {
     this.setState({region: this.state.selectedRegion, ruleset: next.state.ruleSet})
   }
   render() {
-    this.handleGet();
     return (
       <div>
         <div className="header">
-          <img className="alpaca" />
+          <img alt="Application Logo" className="alpaca" />
           <span className="header-title">CODE: MAMMAL</span>
-          <img className="shield" />
+          <img alt="Enterprise Logo" className="shield" />
         </div>
         {this.state.login ?
           <form className="login">
             <Col md={4} />
             <Col md={4}>
-            <FormGroup
-              controlId="username"
-            >
-              <FormControl
-                type="text"
-                value={this.state.value}
-                placeholder="Username"
-                onChange={this.handleChange}
-              />
-              <FormControl.Feedback />
-            </FormGroup>
               <FormGroup
-              controlId="password"
-            >
-              <FormControl
-                type="text"
-                value={this.state.value}
-                placeholder="Password"
-                onChange={this.handleChange}
-              />
-            </FormGroup>
+                controlId="username"
+              >
+                <FormControl
+                  type="text"
+                  value={this.state.value}
+                  placeholder="Username"
+                  onChange={this.handleChange}
+                />
+                <FormControl.Feedback />
+              </FormGroup>
+              <FormGroup
+                controlId="password"
+              >
+                <FormControl
+                  type="text"
+                  value={this.state.value}
+                  placeholder="Password"
+                  onChange={this.handleChange}
+                />
+              </FormGroup>
               <Button style={{width: '250px'}} bsSize="small" onClick={this.handleLogin}>Login</Button>
-          </Col>
+            </Col>
           </form>
           :
           <div>
@@ -104,64 +104,66 @@ class VillainDataEntry extends Component {
               <Row>
                 <Col md={4} />
                 <Col md={4}>
-                <DropdownButton title={this.state.selectedRegion ? this.state.selectedRegion : 'Select a Region'} id="bg-nested-dropdown">
-                  {
-                    this.state.regions.map((region) => {
-                      return <MenuItem onClick={ () =>
-                        this.setState({
-                          selectedRegion: region,
-                          choosingRegion: false
-                        })
-                      } key={region} >{region}</MenuItem>
-                    })
-                  }
-                </DropdownButton>
-                  <Modal show={this.state.showModal} onHide={this.close}>
-                    <Modal.Header closeButton>
-                      <Modal.Title>Modal heading</Modal.Title>
-                    </Modal.Header>
-                    <Modal.Body>
-                      <h4>Text in a modal</h4>
-                      <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula.</p>
-                    </Modal.Body>
-                    <Modal.Footer>
-                      <Button onClick={this.close}>Close</Button>
-                    </Modal.Footer>
-                  </Modal>
+                  <DropdownButton title={this.state.selectedRegion ? this.state.selectedRegion : 'Select a Region'} id="bg-nested-dropdown">
+                    {
+                      this.state.regions.map((region) => {
+                        return <MenuItem onClick={ () =>
+                          this.setState({
+                            selectedRegion: region,
+                            choosingRegion: false
+                          })
+                        } key={region} >{region}</MenuItem>
+                      })
+                    }
+                  </DropdownButton>
+                  <Modal title='Modal title' onRequestHide={this.hideListItem}>
+                    <p>I am a modal!</p>
+                  </Modal>{/*<Modal show={this.state.showModal} onHide={this.close}>
+                 <Modal.Header closeButton>
+                 <Modal.Title>Modal heading</Modal.Title>
+                 </Modal.Header>
+                 <Modal.Body>
+                 <h4>Text in a modal</h4>
+                 <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula.</p>
+                 </Modal.Body>
+                 <Modal.Footer>
+                 <Button onClick={this.close}>Close</Button>
+                 </Modal.Footer>
+                 </Modal>*/}
                 </Col>
               </Row> :
-            <Row>
-              <Col md={4} />
-              <Col md={4}>
-                <div class="button-wrapper">
-                  <Button bsStyle="info" onClick={this.viewTheVillian}>View Villian</Button>
-                </div>
-                {this.state.viewVillian ?
-                  <div>
-                    <Row>
-                      <Well bsSize="small"><span style={{marginRight: '100px'}}>First Name:</span>Curtis</Well>
-                      <Well bsSize="small"><span style={{marginRight: '100px'}}>Last Name:</span>Meuth</Well>
-                      <Well bsSize="small"><span style={{marginRight: '100px'}}>Gender:</span>Female</Well>
-                      <Well bsSize="small"><span style={{marginRight: '100px'}}>Power Level:</span>-1</Well>
-                      <Well bsSize="small"><span style={{marginRight: '100px'}}>Alter Ego:</span>El Corgo</Well>
-                      { this.state.ruleSet[this.state.selectedRegion]['allowDataRemoval'] ? <Button bsStyle="danger">Delete</Button> : ''}
-                    </Row>
-                  </div>
-                  : ''}
-                {!this.state.viewVillian ?
+              <Row>
+                <Col md={4} />
+                <Col md={4}>
                   <div className="button-wrapper">
-                    <Button style={{marginBottom: '5px'}} bsStyle="info">Add Villian</Button>
+                    <Button bsStyle="info" onClick={this.viewTheVillian}>View Villian</Button>
                   </div>
-                :
-                  '' }
+                  {this.state.viewVillian ?
+                    <div>
+                      <Row>
+                        <Well bsSize="small"><span style={{marginRight: '100px'}}>First Name:</span>Curtis</Well>
+                        <Well bsSize="small"><span style={{marginRight: '100px'}}>Last Name:</span>Meuth</Well>
+                        <Well bsSize="small"><span style={{marginRight: '100px'}}>Gender:</span>Female</Well>
+                        <Well bsSize="small"><span style={{marginRight: '100px'}}>Power Level:</span>-1</Well>
+                        <Well bsSize="small"><span style={{marginRight: '100px'}}>Alter Ego:</span>El Corgo</Well>
+                        { this.state.ruleSet[this.state.selectedRegion]['allowDataRemoval'] ? <Button bsStyle="danger">Delete</Button> : ''}
+                      </Row>
+                    </div>
+                    : ''}
+                  {!this.state.viewVillian ?
+                    <div className="button-wrapper">
+                      <Button style={{marginBottom: '5px'}} bsStyle="info">Add Villian</Button>
+                    </div>
+                    :
+                    '' }
                   <Row>
                     {this.state.ruleSet[this.state.selectedRegion]['regionalPrivacy'] ? <Button style={{marginBottom: '5px'}}>Privacy Policy</Button> : ''}
                   </Row>
                   <Row>
                     {this.state.ruleSet[this.state.selectedRegion]['allowComplaints'] ? <Button style={{marginBottom: '5px'}}>Complaint Department</Button> : ''}
                   </Row>
-              </Col>
-            </Row> }
+                </Col>
+              </Row> }
           </div>
 
         }
