5809d2189b4a5a664e6731e1ca9c86421faa81fb
Lauren Hilton <lauren.hilton@daugherty.com>
2017-10-01 10:48:16 -0500

    over 18 prompt

--- src/containers/VillainDataEntry.js
+++ src/containers/VillainDataEntry.js
@@ -1,20 +1,12 @@
 import React, { Component } from 'react';
-import { Button, DropdownButton, Col, Image, FormControl, FormGroup, Modal, MenuItem, Row, Well } from 'react-bootstrap'
+import { Button, DropdownButton, Col, Image, FormControl, FormGroup, ButtonGroup, MenuItem, Row, Well } from 'react-bootstrap'
 import curtis from '../curtis.jpg';
 
 class VillainDataEntry extends Component {
   constructor(props) {
     super(props);
-    this.state = {login: true, choosingRegion: true, showPrivacyModal: false}
+    this.state = {login: true, choosingRegion: true, showPrivacyModal: false, show18Modal: false}
     this.handleGet();
-  }
-  toggleClick = (event) => {
-    const oldRuleSet = this.state.ruleSet;
-    const changedIdValue = event.target.id;
-    oldRuleSet[this.state.selectedRegion][changedIdValue] = !oldRuleSet[this.state.selectedRegion][changedIdValue];
-    this.setState({
-      oldRuleSet
-    });
   }
 
   closePrivacy = () => {
@@ -23,6 +15,14 @@ class VillainDataEntry extends Component {
 
   openPrivacy = () => {
     this.setState({ showPrivacyModal: true });
+  }
+
+  close18 = () => {
+    this.setState({ show18Modal: false });
+  }
+
+  closeLogout18 = () => {
+    this.setState({ show18Modal: false, login: true, choosingRegion: true, selectedRegion: null });
   }
 
   handleGet = () => {
@@ -56,7 +56,6 @@ class VillainDataEntry extends Component {
 
   viewTheVillian = () => {
     this.setState({viewVillian: true})
-    this.setState({showPrivacyModal: true})
   }
 
   componentWillReceiveProps(next) {
@@ -74,6 +73,16 @@ class VillainDataEntry extends Component {
           <div className="other-modal">
             <div className="other-modal-text">Privacy Policy: Tumblr single-origin coffee portland irure, freegan typewriter cray echo park. Pok pok flexitarian messenger bag, thundercats YOLO hammock vexillologist deserunt +1 tattooed ut helvetica green juice. Pickled offal glossier fingerstache. Nulla yr keytar, wolf truffaut next level chambray tbh. Yr iceland ex, narwhal adaptogen YOLO succulents ad biodiesel. Coloring book nostrud minim green juice offal authentic live-edge. Normcore jean shorts drinking vinegar, magna ipsum put a bird on it heirloom cray asymmetrical occaecat aesthetic disrupt. Lyft quis gluten-free synth dolore messenger bag pickled copper mug DIY. Wayfarers listicle artisan, quis chillwave non kickstarter cold-pressed butcher. Hot chicken polaroid copper mug consectetur. Bespoke microdosing ea, ut chia unicorn laborum drinking vinegar narwhal.</div>
             <Button onClick={this.closePrivacy} className="other-modal-button">Close</Button>
+          </div> : ''
+        }
+        { this.state.show18Modal && this.state.ruleSet[this.state.selectedRegion]['prompt18'] ?
+          <div className="other-modal">
+            <div className="other-modal-text offset-top">This website requires you to be 18 years or older.</div>
+            <div className="other-modal-text large">Are you over 18?</div>
+            <ButtonGroup className="other-modal-button">
+            <Button onClick={this.close18}>Yes</Button>
+            <Button onClick={this.closeLogout18}>No</Button>
+            </ButtonGroup>
           </div> : ''
         }
         {this.state.login ?
@@ -116,7 +125,8 @@ class VillainDataEntry extends Component {
                         return <MenuItem onClick={ () =>
                           this.setState({
                             selectedRegion: region,
-                            choosingRegion: false
+                            choosingRegion: false,
+                            show18Modal: true
                           })
                         } key={region} >{region}</MenuItem>
                       })
